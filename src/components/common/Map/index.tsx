0f103052f420c80d14dd1a5dded7bf7c5575dd34
Auh3b <ngaiyayec@yahoo.com>
2024-11-04 14:38:05 +0200

    change dir for map container

---
+++ src/components/common/Map/index.tsx
@@ -0,0 +1,38 @@
+import { Box } from '@mui/material';
+import { DeckGL } from '@deck.gl/react';
+import { Map } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import getLayers from 'layers';
+import useMapStore from '@storesuseMapStore';
+import useBasemap from '@hooks/useBasemap';
+import useMapHooks from './useMapHooks';
+
+const accessToken = import.meta.env.VITE_MAPBOX_ACCESSTOKEN;
+// const transitionInterpolator = new LinearInterpolator();
+
+export default function MapContainer() {
+  const layers = getLayers();
+  const { viewState, basemapUrl } = useMapStore((state) => state);
+  useBasemap();
+  const { handleResize, handleViewStateChange } = useMapHooks();
+  return (
+    <Box
+      height={'100%'}
+      width={'100%'}>
+      <DeckGL
+        controller
+        layers={layers}
+        onResize={handleResize}
+        // @ts-ignore
+        onViewStateChange={handleViewStateChange}
+        viewState={{
+          ...viewState,
+        }}>
+        <Map
+          mapboxAccessToken={accessToken}
+          mapStyle={basemapUrl}
+        />
+      </DeckGL>
+    </Box>
+  );
+}
