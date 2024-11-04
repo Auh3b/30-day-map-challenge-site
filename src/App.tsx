b1f01feda51b9c3cd34f5a1f6d0db4772eb174d9
Auh3b <ngaiyayec@yahoo.com>
2024-11-04 14:40:39 +0200

    added useChallengeData hook

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ import { useEffect } from 'react';
 import useDayChallenge from '@hooks/useDayChallenge';
 import { RouterProvider } from 'react-router-dom';
 import router from 'routes';
+import useChallengeData from '@hooks/useChallengeData';
 
 function App() {
+  useChallengeData();
   const { setDayChallenge } = useDayChallenge();
   const currentDate = new Date('1 November, 2024');
   // const year = currentDate.getFullYear();
   const month = currentDate.getMonth();
   const day = currentDate.getDate();
+
   useEffect(() => {
     if (month < 10) return setDayChallenge(0);
     if (month > 10) return setDayChallenge(31);
