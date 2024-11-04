e517a0a6aa062482c1aa4f5ba7ca44f500cc90cd
Auh3b <ngaiyayec@yahoo.com>
2024-11-05 01:50:35 +0200

    refined hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,12 @@ import 'App.css';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from 'styles/theme';
 import { CssBaseline } from '@mui/material';
-import { useEffect } from 'react';
-import useDayChallenge from '@hooks/useDayChallenge';
 import { RouterProvider } from 'react-router-dom';
 import router from 'routes';
 import useChallengeData from '@hooks/useChallengeData';
 
 function App() {
   useChallengeData();
-  const { setDayChallenge } = useDayChallenge();
-  const currentDate = new Date('1 November, 2024');
-  // const year = currentDate.getFullYear();
-  const month = currentDate.getMonth();
-  const day = currentDate.getDate();
-
-  useEffect(() => {
-    if (month < 10) return setDayChallenge(0);
-    if (month > 10) return setDayChallenge(31);
-    setDayChallenge(day);
-  }, []);
 
   return (
     <ThemeProvider theme={theme}>
--- src/hooks/useChallengeData.ts
+++ src/hooks/useChallengeData.ts
@@ -1,6 +1,10 @@
 import usePageStore from '@storesusePageStore';
 import { useEffect } from 'react';
 
+import MAP_CHALLENGE_DATA from 'data/30_day_map_challenge_details.json';
+import { MapChallengeData } from 'types/data';
+import { max } from 'd3';
+
 const CHALLENGE_DATA_URL =
   'https://raw.githubusercontent.com/Auh3b/30-day-map-challenge-data/refs/heads/main/outline.json';
 
@@ -11,11 +15,27 @@ async function fetchChallengeData() {
 }
 
 export default function useChallengeData() {
-  const setChallengeData = usePageStore((state) => state.setChallengeData);
+  const { date, setChallengeData, setDayChallengeInfo } = usePageStore(
+    (state) => state,
+  );
 
   useEffect(() => {
     fetchChallengeData()
-      .then((data) => setChallengeData(data))
+      .then((data) => {
+        setChallengeData(data);
+        const day = getLatestOutlineDay(data);
+        return setDayChallengeInfo(MAP_CHALLENGE_DATA[day]);
+      })
       .catch((error) => console.log(error));
   }, []);
+
+  useEffect(() => {
+    setDayChallengeInfo(MAP_CHALLENGE_DATA[date]);
+  }, [date]);
+}
+
+function getLatestOutlineDay(value: MapChallengeData) {
+  const days = Object.keys(value);
+  const latest_day = max(days);
+  return latest_day;
 }
