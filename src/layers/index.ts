92a199f84c78afb72986ff6d0dabdecac0e74d32
Auh3b <ngaiyayec@yahoo.com>
2024-11-05 00:24:01 +0200

    added new layers

--- src/layers/index.ts
+++ src/layers/index.ts
@@ -1,8 +1,10 @@
 import Day1Layer from './Day1Layer';
 import Day2Layer from './Day2Layer';
+import Day3Layer from './Day3Layer';
+import Day4Layer from './Day4Layer';
 
 const getLayers = () => {
-  return [Day1Layer(), Day2Layer()];
+  return [Day1Layer(), Day2Layer(), Day3Layer(), Day4Layer()];
 };
 
 export default getLayers;
