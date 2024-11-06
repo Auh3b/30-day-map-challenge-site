3d0b1fff1558f61098f8e3483a0c8d47c938010b
Auh3b <ngaiyayec@yahoo.com>
2024-11-06 21:18:50 +0200

    added day 6 layer

--- src/layers/index.ts
+++ src/layers/index.ts
@@ -3,9 +3,17 @@ import Day2Layer from './Day2Layer';
 import Day3Layer from './Day3Layer';
 import Day4Layer from './Day4Layer';
 import Day5Layer from './Day5Layer';
+import Day6Layer from './Day6Layer';
 
 const getLayers = () => {
-  return [Day1Layer(), Day2Layer(), Day3Layer(), Day4Layer(), Day5Layer()];
+  return [
+    Day1Layer(),
+    Day2Layer(),
+    Day3Layer(),
+    Day4Layer(),
+    Day5Layer(),
+    Day6Layer(),
+  ];
 };
 
 export default getLayers;
