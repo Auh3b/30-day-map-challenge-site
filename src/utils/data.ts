60c1dd934a1c6b9a63196d1e864143990b8b194c
Auh3b <ngaiyayec@yahoo.com>
2024-11-09 13:20:52 +0200

    added data utility funcs

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,5 +1,15 @@
 import { extent } from 'd3';
 
-export function getRange<T, K>(data: Array<T>, column: K) {
-  return extent(data, (d) => d[column]);
+export function getRangeFromObjectArray<T = { [k: string]: string | number }>(
+  data: Array<T>,
+  column: keyof T,
+) {
+  // @ts-expect-error
+  return getRange(data, column);
+}
+
+export function getRange<T = number>(data: Array<T>, column?: string) {
+  if (column) return extent(data, (d) => d[column]);
+  // @ts-expect-error
+  return extent(data);
 }
